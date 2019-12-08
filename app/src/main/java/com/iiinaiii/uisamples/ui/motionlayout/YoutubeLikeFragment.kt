9c185a1db8e362970256573fbdc45a01605305c9
naoki-ishii <kyoumi155@gmail.com>
2019-12-08 13:40:43 +0900

    Fix compile error

--- app/src/main/java/com/iiinaiii/uisamples/ui/motionlayout/YoutubeLikeFragment.kt
+++ app/src/main/java/com/iiinaiii/uisamples/ui/motionlayout/YoutubeLikeFragment.kt
@@ -1,5 +1,6 @@
 package com.iiinaiii.uisamples.ui.motionlayout
 
 import androidx.fragment.app.Fragment
+import com.iiinaiii.uisamples.R
 
-class YoutubeLikeFragment:Fragment(R.layout.)
+class YoutubeLikeFragment : Fragment(R.layout.fragment_motion_youtube_like)
