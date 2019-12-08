26176b4869ac37cdcdb718dba407049e30c87d1c
naoki-ishii <kyoumi155@gmail.com>
2019-12-08 17:50:42 +0900

    Fix toolbar.

--- app/src/main/java/com/iiinaiii/uisamples/ui/motionlayout/GoogleCalenderLikeFragment.kt
+++ app/src/main/java/com/iiinaiii/uisamples/ui/motionlayout/GoogleCalenderLikeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.iiinaiii.uisamples.R
 import com.iiinaiii.uisamples.ext.addSystemWindowInsetToPadding
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,7 +13,19 @@ class GoogleCalenderLikeFragment : Fragment(R.layout.fragment_google_calendar_li
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         (activity as? AppCompatActivity)?.supportActionBar?.hide()
-        toolbar.addSystemWindowInsetToPadding(top = true)
+
+        toolbar.apply {
+            addSystemWindowInsetToPadding(top = true)
+            setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (activity as? AppCompatActivity)?.supportActionBar?.show()
     }
 }
