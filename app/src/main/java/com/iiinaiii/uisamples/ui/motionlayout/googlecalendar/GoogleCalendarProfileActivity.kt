a44549cb1c0c91f5ef9995422213a1f871804ee3
naoki-ishii <kyoumi155@gmail.com>
2019-12-15 23:54:09 +0900

    Google Calendar screen motion.

--- app/src/main/java/com/iiinaiii/uisamples/ui/motionlayout/googlecalendar/GoogleCalendarProfileActivity.kt
+++ app/src/main/java/com/iiinaiii/uisamples/ui/motionlayout/googlecalendar/GoogleCalendarProfileActivity.kt
@@ -5,14 +5,25 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.appbar.MaterialToolbar
 import com.iiinaiii.uisamples.R
-import com.iiinaiii.uisamples.databinding.ActivityGoogleCalendarProfileBinding
+import com.iiinaiii.uisamples.databinding.ActivityGoogleCalendarProfileCoordinatorBinding
 
 class GoogleCalendarProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<ActivityGoogleCalendarProfileBinding>(this, R.layout.activity_google_calendar_profile)
+        DataBindingUtil.setContentView<ActivityGoogleCalendarProfileCoordinatorBinding>(
+            this,
+            R.layout.activity_google_calendar_profile_coordinator
+        )
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        findViewById<MaterialToolbar>(R.id.content_toolbar)?.apply {
+            inflateMenu(R.menu.menu_reference)
+        }
     }
 
     companion object {
